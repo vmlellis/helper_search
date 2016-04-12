362e24c998f6ba2d2f7f7949f321a9ba088e1ba4
Victor Lellis <vmlellis@gmail.com>
2016-04-12 04:04:31 -0300

    sidekiq initializer

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+url = "redis://redis:6379/0"
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: url }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: url }
+end
--- config/sidekiq.rb
+++
@@ -1,5 +0,0 @@
-ENV["REDIS_URL"] ||= "redis://#{ENV['REDIS_HOST']}:6379"
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDIS_URL"], namespace: 'sidekiq' }
-end
